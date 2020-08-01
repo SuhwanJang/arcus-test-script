eb6d2fc2a96e40784b7bd5311beb042896561755
suhwanJang <baseball9185@naver.com>
2020-08-01 15:31:43 +0900

    check cpu stats before switchover

---
+++ replSetup.sh
@@ -0,0 +1,56 @@
+ZK_CLI="/home/long_running_replication/arcus/zookeeper/bin/zkCli.sh"
+ZK_ADDR="-server 10.34.35.122:2195,10.33.136.247:2195,10.33.137.55:2195"
+
+$ZK_CLI $ZK_ADDR create /arcus_repl 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/client_list 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/client_list/long_running_replication 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_log 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_list 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_list/long_running_replication 0
+# ehpemeral znode = <group>^M^<ip:port-hostname> 0 // created by cache node
+# ehpemeral znode = <group>^S^<ip:port-hostname> 0 // created by cache node
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list/long_running_replication 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list/long_running_replication/g0 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list/long_running_replication/g1 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list/long_running_replication/g2 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list/long_running_replication/g3 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list/long_running_replication/g4 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/group_list/long_running_replication/g5 0
+# ehpemeral/sequence znode = <nodeip:port>^<listenip:port>^<sequence> 0
+# ehpemeral/sequence znode = <nodeip:port>^<listenip:port>^<sequence> 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11700 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11700/long_running_replication^g0^10.34.35.122:20700 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11701 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11701/long_running_replication^g0^10.33.136.247:20701 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11700 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11700/long_running_replication^g1^10.33.136.247:20700 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11701 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11701/long_running_replication^g1^10.33.137.55:20701 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11700 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11700/long_running_replication^g2^10.33.137.55:20700 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11701 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11701/long_running_replication^g2^10.34.35.122:20701 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11800 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11800/long_running_replication^g3^10.34.35.122:20800 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11801 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11801/long_running_replication^g3^10.33.136.247:20801 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11800 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.136.247:11800/long_running_replication^g4^10.33.136.247:20800 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11801 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11801/long_running_replication^g4^10.33.137.55:20801 0
+
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11800 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.33.137.55:11800/long_running_replication^g5^10.33.137.55:20800 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11801 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/10.34.35.122:11801/long_running_replication^g5^10.34.35.122:20801 0
--- switchover.bash
+++ switchover.bash
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+function check_cpustat() {
+  durable=5
+  sum=0
+  requests=3
+  for (( i=0; i<$requests; i++ ))
+  do
+    before=$(echo stats | nc $1 $2 | grep rusage_user | tr -d '\r' | cut -d' ' -f3 | cut -d'.' -f1)
+    if [ -z "$before" ]; then
+      echo "stats failed"
+      return 1;
+    else
+      :
+    fi
+    
+    sleep $durable;
+    
+    after=$(echo stats | nc $1 $2 | grep rusage_user | tr -d '\r' | cut -d' ' -f3 | cut -d'.' -f1)
+    if [ -z "$after" ]; then
+      echo "stats failed"
+      return 1;
+    else
+      :
+    fi
+    
+    diff=$((after - before))
+    res=$(bc <<< "scale=2; $diff / $durable" | cut -d'.' -f2)
+    let "sum+=res"
+  done
+  let "sum/=requests"
+  if [ $sum -gt 70 ]; then
+    echo "high cpu stat host=$1:$2"
+    return 1;
+  fi
+  return 0;
+}
+
 echo ">>>>>> $0 master_port slave_port start_time run_interval"
 
 if [ -z "$1" ];
@@ -41,31 +77,35 @@ echo ">>>>>> $0 $master_port $slave_port $start_time $run_interval"
 
 can_test_failure="switchover_${master_port}.txt"
 if ! [ -f "$can_test_failure" ]; then
-    touch $can_test_failure
+  touch $can_test_failure
 fi
 
 echo ">>>>>> sleep for $start_time before switchover"
 sleep $start_time
 
 file_time=0
-slave_hostname="jam2in-s004"
+slave_hostname="jam2in-s001"
 
 COUNTER=1
 #while [ $COUNTER -le $run_count ];
 while true;
-do 
-  echo ">>>>>> $0 running ($COUNTER)"
+do
+  echo ">>>>>> $0 running (COUNT=$COUNTER)"
   if  [ -f "$can_test_failure" ];
   then
     if  [ `expr $COUNTER % 2` == 1 ];
     then
-      echo ">>>>>> execute switchover : $master_port"
-      echo "replication switchover" | nc localhost $master_port 2> $can_test_failure
+      if check_cpustat "127.0.0.1" $master_port; then
+        echo ">>>>>> execute switchover host=127.0.0.1:$master_port"
+        echo "replication switchover" | nc localhost $master_port 2> $can_test_failure
+      fi
     else
-      echo ">>>>>> execute switchover : $slave_port"
-      ssh $slave_hostname /bin/bash << EOF
+      if check_cpustat $slave_hostname $slave_port; then
+        echo ">>>>>> execute switchover host=$slave_hostname:$slave_port"
+        ssh $slave_hostname /bin/bash << EOF
         echo "replication switchover" | nc localhost $slave_port 2> $can_test_failure
 EOF
+      fi
     fi
   else
     echo ">>>>>> cannot switchover (test case ended)"
@@ -74,6 +114,6 @@ EOF
   echo ">>>>>> sleep for $run_interval"
   sleep $run_interval
   echo ">>>>>> wakeup"
-
+  
   let COUNTER=COUNTER+1
 done
