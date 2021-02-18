016a0db76192b180271ef02ff4cd8b38c52fc89a
Johyeyoung <missenergy@naver.com>
2021-02-18 18:07:18 +0900

    used /home/persistence macro

--- run_memtier.sh
+++ run_memtier.sh
@@ -9,6 +9,7 @@ protocol=$(echo $1 | cut -d '/' -f 6) # memcache_text/ redis
 client_mode=$(echo $1 | cut -d '/' -f 7 | cut -d '(' -f 1)
 requests=$((keymaximum/(clients*threads)))
 key_median=$(((keymaximum-keyminimum)/2))
+port=$2
 
 # define server IP(현재client의IP를 확인해서)
 server=$(hostname -I | tr -d '[:space:]')
@@ -16,28 +17,26 @@ case ${server} in
 10.34.93.160) remote="11618";; # server: m001 & client: m002
 10.34.91.143) remote="11617";; # server: m002 & client: m001
 esac
-# check account
-account=$(pwd | cut -d '/' -f 3)
-port=$2
+
 #read client_mode
 case $client_mode in
 1) cli_mode="[1] onlySet"
 C_command="ssh -T persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} \
 --data-size=${data_size} --key-pattern=P:P --key-minimum=${keyminimum} --key-maximum=${keymaximum} \
 --ratio=1:0 --requests=${requests} --print-percentiles=90,95,99 --show-config" ;;
 
 2) cli_mode="[2] onlyGetRandom"
 C_command="ssh -T persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --key-pattern=R:R \
 --distinct-client-seed --randomize --key-minimum=${keyminimum} --key-maximum=${keymaximum} \
 --ratio=0:1 --requests=${requests} --print-percentiles=90,95,99 --show-config" ;;
 
 3) cli_mode="[3] onlyGetLongtail"
 C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --key-pattern=G:G \
 --distinct-client-seed --randomize --key-minimum=${keyminimum} --key-maximum=${keymaximum} \
 --ratio=0:1 --requests=${requests} \
@@ -45,7 +44,7 @@ C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
 
 4) cli_mode="[4] GetSetRandom(1:9)"
 C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --data-size=${data_size} \
 --key-pattern=P:P --distinct-client-seed --randomize --key-minimum=${keyminimum} \
 --key-maximum=${keymaximum} --ratio=1:9 --requests=${requests} \
@@ -53,7 +52,7 @@ C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
 
 5) cli_mode="[4] GetSetRandom(3:7)"
 C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --data-size=${data_size} \
 --key-pattern=P:P --distinct-client-seed --randomize --key-minimum=${keyminimum} \
 --key-maximum=${keymaximum} --ratio=3:7 --requests=${requests} \
@@ -61,7 +60,7 @@ C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
 
 6) cli_mode="[4] GetSetRandom(5:5)"
 C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --data-size=${data_size} \
 --key-pattern=P:P --distinct-client-seed --randomize --key-minimum=${keyminimum} \
 --key-maximum=${keymaximum} --ratio=5:5 --requests=${requests} \
@@ -69,7 +68,7 @@ C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
 
 7) cli_mode="[5] GetSetLongtail(1:9)"
 C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --data-size=${data_size} \
 --key-pattern=G:G --distinct-client-seed --randomize --key-minimum=${keyminimum} \
 --key-maximum=${keymaximum} \
@@ -77,7 +76,7 @@ C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
 
 8) cli_mode="[5] GetSetLongtail(3:7)"
 C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --data-size=${data_size} \
 --key-pattern=G:G --distinct-client-seed --randomize --key-minimum=${keyminimum} \
 --key-maximum=${keymaximum} \
@@ -85,7 +84,7 @@ C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
 
 9) cli_mode="[5] GetSetLongtail(5:5)"
 C_command="ssh -t persistence@211.249.63.38 -p ${remote} \
-/home/${account}/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
+$HOME/memtier_benchmark/memtier_benchmark -s ${server} -p ${port} \
 --protocol=${protocol} --threads=${threads} --clients=${clients} --data-size=${data_size} \
 --key-pattern=G:G --distinct-client-seed --randomize --key-minimum=${keyminimum} \
 --key-maximum=${keymaximum} \
